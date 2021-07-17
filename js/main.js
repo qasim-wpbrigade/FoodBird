b1aa59c99497eb6e82918e6252c3d3c3b2efbbe6
qasim-wpbrigade <87133962+qasim-wpbrigade@users.noreply.github.com>
2021-07-17 17:13:56 +0500

    update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(".slider").owlCarousel({
+    items: 1,
+    lazyLoad: true,
+    loop: true,
+  });
+  var owl = $(".slider");
+  // Go to the next item
+  $(".next").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
+  // Go to the previous item
+  $(".prev").click(function () {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger("prev.owl.carousel", [300]);
+  });
+});
